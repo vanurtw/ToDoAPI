bf3e81147b89be9bf3eb1e78e591f31596144494
vano <vanuartw@mail.ru>
2024-03-15 12:27:40 +0300

    create user

--- ToDoAPI/api/serializers.py
+++ ToDoAPI/api/serializers.py
@@ -1,14 +1,32 @@
 from rest_framework import serializers
 from .models import Task
+from django.contrib.auth.models import User
 
 
 class TaskSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=255)
-    content = serializers.CharField()
-    completed = serializers.BooleanField(read_only=True)
+    title = serializers.CharField(max_length=255, read_only=True)
+    content = serializers.CharField(read_only=True)
+    completed = serializers.BooleanField(default=False)
     data_create = serializers.DateTimeField(read_only=True)
     data_update = serializers.DateTimeField(read_only=True)
     user = serializers.CharField(source='author.username', read_only=True)
 
     def create(self, validated_data):
         return Task.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.__dict__.update(**validated_data)
+        instance.save()
+        return instance
+
+
+class UserSerializers(serializers.Serializer):
+    username = serializers.CharField(max_length=200)
+    email = serializers.EmailField(read_only=True)
+    password = serializers.CharField(max_length=255)
+
+    def create(self, validated_data):
+        user = User(**validated_data)
+        user.set_password(validated_data.get('password'))
+        user.save()
+        return user
--- ToDoAPI/api/urls.py
+++ ToDoAPI/api/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('task-all/', views.TaskListView.as_view()),
+    path('task-update/<int:pk>/', views.TaskListView.as_view()),
     path('user-auth/', include('rest_framework.urls')),
+    path('user-register/', views.UserRegister.as_view()),
 
 ]
